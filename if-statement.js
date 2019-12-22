a39f1ee309dbf8e26067372ec865f8d0c4f41d48
Ken Fyfe <kenfyfe@kenfyfe.com>
2019-12-22 20:22:20 +0000

    added a bunch of more files

--- if-statement.js
+++ if-statement.js
@@ -1,6 +1,6 @@
 const fruit = 'orange'
 if (fruit.length > 5) {
-  console.log('> 5')
+  console.log('The fruit name has more than five characters.')
 } else {
-  console.log('< 5')
+  console.log('The fruit name has five characters or less.')
 }
